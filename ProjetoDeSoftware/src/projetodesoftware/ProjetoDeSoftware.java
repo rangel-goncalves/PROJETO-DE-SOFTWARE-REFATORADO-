e77dd936157f871d452986927ef3e16d50c52e6a
Rangel <Rangel@DESKTOP-E14RECD>
2021-10-29 17:24:32 -0300

    PROJETO-DE-SOFTWARE1

--- ProjetoDeSoftware/src/projetodesoftware/ProjetoDeSoftware.java
+++ ProjetoDeSoftware/src/projetodesoftware/ProjetoDeSoftware.java
@@ -16,8 +16,7 @@ public class ProjetoDeSoftware {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("foi");
-        System.out.println("foi");
+        System.out.println("github");
     }
     
 }
