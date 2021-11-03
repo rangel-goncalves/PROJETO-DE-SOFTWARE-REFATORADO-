f1636ded9c077f6473cc6a916adb7cb373598fda
Rangel <Rangel@DESKTOP-E14RECD>
2021-11-03 17:03:08 -0300

    Quadro de funcionarios, mudança de dados, retirada de algumas areas comentadas

--- ProjetoDeSoftware/src/projetodesoftware/ProjetoDeSoftware.java
+++ ProjetoDeSoftware/src/projetodesoftware/ProjetoDeSoftware.java
@@ -14,49 +14,13 @@ import java.util.Scanner;
  */
 public class ProjetoDeSoftware {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner input =  new Scanner(System.in);
-        /*ArrayList<Funcionario> f = new ArrayList();
-        String name ;
-        String address;
-        int employeeID;
-        String type;
     
-        float salary;
-        float commission = 0.0f;
-        boolean syndicate;
+    public static void main(String[] args) {
         
-        /*System.out.println("Adicionar Funcionario");
-        System.out.println("Dados do Funcionario:");
-        System.out.println("Nome: ");
-        name= input.nextLine();
-        System.out.println("Endereço: ");
-        address = input.nextLine();
-        //input.nextLine();
-        System.out.println("CPF: ");
-        employeeID = input.nextInt();
-        input.nextLine();
-        System.out.println("Tipo:(hourly, salaried, commissioned)");
-        type = input.nextLine();
-        if(type.equals("commissioned")){
-            System.out.println("Percentual de comissão: ");
-            commission = input.nextFloat();
-        }
-        System.out.println("Salário: ");
-        salary = input.nextFloat();
-        System.out.println("É afiliado ao sindicato?(1 para S/0 para N)");
-        syndicate = input.nextBoolean();
-        f.add(new Funcionario(name,address,employeeID,type,salary,commission,syndicate));*/
+        Scanner input =  new Scanner(System.in);
+       
         QuadrodeFuncionarios qf = new QuadrodeFuncionarios();
         
-        //qf.add();
-        //qf.add();
-        //qf.add();
-        //qf.pesquisar();
         while(true){
             System.out.println("""
                                MENU:
@@ -78,6 +42,7 @@ public class ProjetoDeSoftware {
             if (opcao == 0){
                 break;
             }
+            
         }
     }
     
