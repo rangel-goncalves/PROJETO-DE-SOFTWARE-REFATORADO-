f51b9a57f2bae35d374bcedcb33aa32689afa15c
Rangel <Rangel@DESKTOP-E14RECD>
2021-12-17 12:29:37 -0300

    MAIN
    Retirei a adição de 2 funcionario na interface(resto ta igual)
    Funcionando:
    Adição de um empregado
    Remoção de um empregado
    Lançar um Cartão de Ponto
    Lançar um Resultado Venda
    Lançar uma taxa de serviço
    Alterar detalhes de um empregado
    Rodar a folha de pagamento para hoje
    Agenda de Pagamento
    Agenda de Pagamento
    o Empregados podem escolher o método de pagamento. 
    o Podem receber um cheque pelos correios 
    o Podem receber um cheque em mãos 
    o Podem pedir depósito em conta bancária
    Defalts dos funcionarios

--- ProjetoDeSoftware/src/projetodesoftware/ProjetoDeSoftware.java
+++ ProjetoDeSoftware/src/projetodesoftware/ProjetoDeSoftware.java
@@ -24,22 +24,7 @@ import java.util.Scanner;
  * @author Rangel
  */
 public class ProjetoDeSoftware {
-    /*
-    ***(feitoeu acho TESTE DENOVO)verificar se a agenda existe antes(quase feito, dando proplema no existeagenda com "mensal" e "mensalmente")
-    ***concertar a undo
-    *** (TA VERIFICADO MAS REFORÇA)ver se estar trocando a agenda direito(não esta funcionando a parte de verificar existencia)
-    *** interfasse agenda de pagamento(ja me parece boa)
-    *** undo undo undo carai
-                      ***TEEEEEEEEEEESTARRRRRRRRRR ***
-    
-    */
-    /*                      ARRUMADOS MAS SOB VIGILANCIA
-    ***arrumar a cobrança das taxas de serviços pra gem não recebe mensalmente (ideia: por um bool taxalançada, se true cobra 
-    na agenda de pagamento se false cobra 0(tentei arrumar com o local date em  pagamentoComissionado tem o comentario)
-    ***criar uma função de lançar taxas no quadro de funcionarios
-    *** na interface em edtemployee falta arrumar os metodos de pagamento
-    */
-    
+
     public static void main(String[] args) {
         AdminMain mainMenu =  new AdminMain();
         mainMenu.setVisible(true);
